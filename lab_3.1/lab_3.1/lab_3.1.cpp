aed4646e5ace6a00a73990058b7abbc16329b7e0
Edward Gogichaev <101045057+sAX1E@users.noreply.github.com>
2022-12-16 12:29:57 +0300

    Update lab_3.1.cpp

--- lab_3.1/lab_3.1/lab_3.1.cpp
+++ lab_3.1/lab_3.1/lab_3.1.cpp
@@ -1,145 +1,105 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
-
-void Task1(int* arrt, int sizet) {
-	int zamena = 0;
-	for (int i = 0; i < sizet; i++) {
-		for (int j = i + 1; j < sizet; j++) {
-			if (arrt[i] > arrt[j]) {
-				zamena = arrt[i];
-				arrt[i] = arrt[j];
-				arrt[j] = zamena;
-			}
-
-		}
-	}
+int* case_1(int*& a, int& size) {
+    cout << " Сколько элементов в массиве должно быть: ";
+    cin >> size;
+    delete[] a;
+    int* arr = new int[size];
+    cout << "Введите значения элементов массива: " << endl;
+    for (int i = 0; i < size; i++)
+        cin >> arr[i];
+    //arr[i] = i;
+    return arr;
 }
 
-void Task2(char* arr3, int sizet)
-{
-	int alp[26] = { 0 };
-	const int alb = 26;
-	int c[alb] = { 0 };
-	for (int i = 0; i < sizet; i++) {
-		alp[int(arr3[i]) - 'a']++;
-	}
-	int j = 0;
-	for (int i = 0; i < 26; i++) {
-		while (alp[i] != 0) {
-			arr3[j] = char(i + 'a');
-			j++;
-			alp[i]--;
-		}
-
-	}
+void case_2(int* a, int size) {
+    for (int i = 0; i < size; i++)
+        cout << a[i] << " ";
+    cout << endl;
+}
+int sum(int n) {
+    int k = 0, ch = 0;
+    while (n > 0) {
+        k++;
+        if (!(k % 2))
+            ch += n % 10;
+        n /= 10;
+    }
+    return ch;
+}
+void case_3(int* a, int size) {
+    int t;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (sum(a[j]) > sum(a[j + 1])) {
+                t = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = t;
+            }
+        }
+    }
+    for (int i = 0; i < size; i++)
+        cout << a[i] << " ";
+    cout << endl;
+}
+void case_4(int* a, int size) {
+    int t;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (a[j] % 10 > a[j + 1] % 10) {
+                t = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = t;
+            }
+        }
+    }
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - 1; j++) {
+            if (a[j] % 10 == a[j + 1] % 10 and (a[j] < a[j + 1])) {
+                t = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = t;
+            }
+        }
+    }
+    for (int i = 0; i < size; i++)
+        cout << a[i] << " ";
+    cout << endl;
 }
 
-void mergesort(int* a, int l, int r) {
-	int middle, startl, startr, i;
-	int mas[1000] = { 0 };
-	middle = (l + r) / 2; 
-	startl = l;
-	startr = middle + 1;
-	cout << endl;
-	for (i = l; i < r + 1; i++) {
-		if ((startl < middle) && (startr < r + 1)) {
-			if (a[startl] < a[startr]) {
-				mas[i] = a[startl];
-				startl++;
-			}
-			else {
-				mas[i] = a[startr];
-				startr++;
-			}
-		}
-		else {
-			if (startl < middle + 1) {
-				mas[i] = a[startl];
-				startl++;
-			}
-			else {
-				mas[i] = a[startr];
-				startr++;
-			}
-		}
-	}
-	for (i = l; i < r + 1; i++) a[i] = mas[i];
-}
-
-void Task3(int* a, int l, int r) {
-	if (r - l < 1) {
-		Task3(a, l, (l + r) / 2);
-		Task3(a, (l + r) / 2 + 1, r);
-		mergesort(a, l, r); 
-	}
-	else {
-		if (l == r) return;
-		else {
-			if (a[r] < a[l]) swap(a[r], a[l]);
-		}
-	}
-}
 
 int main() {
-	int choice = 0, size = 0;
+    setlocale(LC_ALL, "Russian");
+    int choise, size = 0;
+    int* a = new int[size];
+    a = case_1(a, size);
+    while (1) {
+        cout << " Что вы хотите выполнить? " << endl <<
+            "1. Ввод массива. " << endl <<
+            "2. Вывод массива в консоль. " << endl <<
+            "3. Сортировка по сумме цифр, стоящих на четных местах. " << endl <<
+            "4. Сортировка массива по последней цифре." << endl <<
+            "5. Выход. " << endl;
 
-	int arr1[1000];
-	char arr2[1000];
-	while (true) {
-		cout << "What task do you want to do? \n1.Task \n2.Task \n3.Task \n4.Exit" << endl;
-		cin >> choice;
-		switch (choice) {
-		case 1:
-		{
-			cout << "input size of your array ";
-			cin >> size;
-			cout << "Print array: ";
-			for (int i = 0; i < size; i++) {
-				cin >> arr1[i];
-			}
-			Task1(arr1, size);
-			for (int i = 0; i < size; i++) {
-				cout << arr1[i] << " ";
-			}
-			cout << endl;
-			break;
-		}
-		case (2):
-		{
-			cout << "input size of your array ";
-			cin >> size;
-			for (int i = 0; i < size; i++)
-			{
-				cin >> arr2[i];
-			}
-			cout << endl;
-			Task2(arr2, size);
-			for (int i = 0; i < size; i++) {
-				cout << arr2[i] << " ";
-			}
-			cout << endl;
-			break;
-		}
-		case 3:
-		{
-			cout << "input size of your array ";
-			cin >> size;
-			cout << "Print array: ";
-			for (int i = 0; i < size; i++) {
-				cin >> arr1[i];
-			}
-
-			Task3(arr1, 0, size - 1);
-			for (int i = 0; i < size; i++) {
-				cout << arr1[i];
-			}
-			cout << endl;
-			break;
-		}
-		default:
-		{
-			return 0;
-		}
-		}
-	}
+        cin >> choise;
+        switch (choise) {
+        case 1: {
+            a = case_1(a, size);
+            break; }
+        case 2: {
+            case_2(a, size);
+            break; }
+        case 3: {
+            case_3(a, size);
+            break; }
+        case 4: {
+            case_4(a, size);
+            break; }
+        case 5: exit(0);
+        default: std::cout << "Wrong number";
+            break;
+        }
+    }
+    delete[] a;
+    return 0;
 }
