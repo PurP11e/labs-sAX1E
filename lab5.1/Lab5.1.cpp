a09115774bfe90dfd531cb5f89f98bb2165cca1d
Red Bread <redbready@gmail.com>
2023-01-19 13:51:17 +0300

    221_351_Gogichaev

--- lab5.1/Lab5.1.cpp
+++ lab5.1/Lab5.1.cpp
@@ -1,149 +1,163 @@
-// Policarpov
-#include "Matrix.h"
-#include <iostream>
+#include<iostream>
+#include"Matrix.h"
+
 int main() {
-	setlocale(LC_ALL, "RUS");
-	Matrix mat1, mat2;
-	int choice;
-	//меню
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	int choise = 0;
+
+	Matrix mat1(4);
+	Matrix mat2(4);
 	while (true) {
-		std::cout << "\nЧто вы хотите выполнить? \n"
-			<< "1. Ввод матрицу №1\n"
-			<< "2. Ввод матрицы №2\n"
-			<< "3. Вывод матрицы №1\n"
-			<< "4. Вывод матрицы №2\n"
-			<< "5. Вывести кол-во строк и столбцов первой матрицы\n"
-			<< "6. Вывести кол-во строк и столбцов второй матрицы\n"
-			<< "7. Вывод элемента матрицы номер 1 по индексу\n"
-			<< "8. Вывод элемента матрицы номер 2 по индексу\n"
-			<< "9. Умножить матрицу №1 на число n\n"
-			<< "10.Умножить матрицу №2 на число n\n"
-			<< "11.Прибавить к матрице №1 матрицу №2\n"
-			<< "12.Найти след первой матрицы\n"
-			<< "13.Найти след второй матрицы\n"
-			<< "14.Умножить первую матрицу на вторую\n"
-			<< "15.Умножить вторую матрицу на первую\n"
-			<< "16.Выход\n";
-		std::cin >> choice;
-		switch (choice) {
+		std::cout << "Какую команду выполнить: \n";
+		std::cout << "1 - Ввод матрицы\n2 - печать матрицы\n3 - печать кол-ва строк матрицы\n4 - печать кол-ва столбцов матрицы\n5 - печать (i, j) элемента\n";
+		std::cout << "6 - сумма матриц\n7 - умножение матриц\n8 - печать следа матрица\n9 - печать определителя матрицы\n10 - умножение матрицы на число\n11 - выход\n";
+		std::cout << "Введите номер команды: "; std::cin >> choise;
+		switch (choise) {
 		case 1: {
-			std::cout << "Ввод матрицы №1\n";
-			mat1.input();
-			std::cout << "Ввод матрицы №1 завершён\n";
+			system("cls");
+			std::cout << "Какую матрицу заполнить?\n1 - первую\n2 - вторую\n";
+			int ans; std::cin >> ans;
+			std::cout << "Введите размер матрицы: \nКол-во строк - ";
+			int rw; std::cin >> rw; std::cout << "Кол-во столбиков - "; int cl; std::cin >> cl;
+			if (ans == 1) {
+				mat1.createmat(rw, cl);
+			}
+			else {
+				mat2.createmat(rw, cl);
+			}
+			std::cout << "Матрица создана\n\n";
 			break;
 		}
 		case 2: {
-			std::cout << "Ввод матрицы №2\n";
-			mat2.input();
-			std::cout << "Ввод матрицы №2завершён\n";
+			system("cls");
+			std::cout << "Какую матрицу вывести?\n1 - первую\n2 - вторую\n";
+			int ans; std::cin >> ans;
+			if (ans == 1) {
+				mat1.print();
+			}
+			else {
+				mat2.print();
+			}
+			std::cout << "\n";
 			break;
 		}
 		case 3: {
-			std::cout << "Вывод матрицы №1\n";
-			mat1.print();
+			system("cls");
+			std::cout << "Кол-во строк какой матрицы вывести?\n1 - первой\n2 - второй\n";
+			int ans; std::cin >> ans;
+			if (ans == 1) {
+				std::cout << "Кол-во строк первой матрицы = " << mat1.get_rows() << "\n\n";
+			}
+			else {
+				std::cout << "Кол-во строк первой матрицы = " << mat2.get_rows() << "\n\n";
+			}
 			break;
 		}
 		case 4: {
-			std::cout << "Вывод матрицы №2\n";
-			mat2.print();
+			system("cls");
+			std::cout << "Кол-во колонок какой матрицы вывести?\n1 - первой\n2 - второй\n";
+			int ans; std::cin >> ans;
+			if (ans == 1) {
+				std::cout << "Кол-во колонок первой матрицы = " << mat1.get_columns() << "\n\n";
+			}
+			else {
+				std::cout << "Кол-во колонок первой матрицы = " << mat2.get_columns() << "\n\n";
+			}
 			break;
 		}
 		case 5: {
-			std::cout << "Кол-во строк первой матрицы " << mat1.get_rows() << '\n';
-			std::cout << "Кол-во столбцов первой матрицы " << mat1.get_columns();
+			system("cls");
+			std::cout << "Из какой матрицы вывести (i, j) элемент?\n1 - из первой\n2 - из второй\n";
+			int ans; std::cin >> ans;
+			if (ans == 1) {
+				std::cout << "Введите номер строки(число от 0 до " << mat1.get_rows() - 1 << "): "; int ans_i; std::cin >> ans_i;
+				std::cout << "Введите номер колонки(число от 0 до " << mat1.get_columns() - 1 << "): "; int ans_j; std::cin >> ans_j;
+				std::cout << "Матрица: \n";
+				mat1.print();
+				std::cout << "Элемент на этой позиции = " << mat1.get_elem(ans_i, ans_j);
+			}
+			else {
+				std::cout << "Введите номер строки(число от 0 до " << mat2.get_rows() - 1 << "): "; int ans_i; std::cin >> ans_i;
+				std::cout << "Введите номер колонки(число от 0 до " << mat2.get_columns() - 1 << "): "; int ans_j; std::cin >> ans_j;
+				std::cout << "Матрица: \n";
+				mat2.print();
+				std::cout << "Элемент на этой позиции = " << mat2.get_elem(ans_i, ans_j);
+			}
+			std::cout << "\n\n";
 			break;
 		}
 		case 6: {
-			std::cout << "Кол-во строк второй матрицы " << mat2.get_rows() << '\n';
-			std::cout << "Кол-во столбцов второй матрицы " << mat2.get_columns();
+			system("cls");
+			mat1.sum(mat2);
+			std::cout << "\n\n";
 			break;
 		}
 		case 7: {
-			int i, j;
-			std::cout << "\nВведите i-индекс и j- индекс для поиска элемента в первой матрице\n";
-			std::cout << "i-индекс: "; std::cin >> i;
-			std::cout << "j-индекс: "; std::cin >> j;
-			std::cout << mat1.get_elem(i, j);
+			system("cls");
+			mat1.mult(mat2);
+			std::cout << "\n\n";
 			break;
 		}
 		case 8: {
-			int i, j;
-			std::cout << "\nВведите i-индекс и j- индекс для поиска элемента во второй матрице\n";
-			std::cout << "i-индекс: "; std::cin >> i;
-			std::cout << "j-индекс: "; std::cin >> j;
-			std::cout << mat2.get_elem(i, j);
+			system("cls");
+			std::cout << "След какой матрицы вывести?\n1 - след первой матрицы\n2 - след второй матрицы\n";
+			int ans; std::cin >> ans;
+			system("cls");
+			if (ans == 1) {
+				mat1.trace();
+			}
+			else {
+				mat2.trace();
+			}
+			std::cout << "\n";
 			break;
 		}
 		case 9: {
-			std::cout << "\nВведите число n: "; double n; std::cin >> n;
-			mat1.mult_by_num(n);
+			system("cls");
+			std::cout << "Определитель какой матрицы вывести?\n1 - определитель первой матрицы\n2 - определитель второй матрицы\n";
+			int ans; std::cin >> ans;
+			system("cls");
+			if (ans == 1) {
+				int f = mat1.get_columns();
+				mat1.det(f);
+			}
+			else {
+				int f = mat2.get_columns();
+				mat2.det(f);
+			}
+			std::cout << "\n";
 			break;
 		}
 		case 10: {
-			std::cout << "\nВведите число n: "; double n; std::cin >> n;
-			mat2.mult_by_num(n);
+			system("cls");
+			std::cout << "Введите число на которое умножить матрицу: ";
+			int k;  std::cin >> k;
+			std::cout << "Какую матрицу умножить на " << k << "?\n1 - первую\n2 - вторую\n";
+			int ans; std::cin >> ans;
+			if (ans == 1) {
+				std::cout << "Матрица до умножения: \n";
+				mat1.print();
+				Matrix mat3(mat1.mult_by_num(k));
+				std::cout << "Матрица после умножения:\n";
+				mat3.print();
+			}
+			else {
+				std::cout << "Матрица до умножения: \n";
+				mat2.print();
+				Matrix mat3(mat2.mult_by_num(k));
+				std::cout << "Матрица после умножения:\n";
+				mat3.print();
+			}
 			break;
 		}
 		case 11: {
-			if (mat2.get_columns() != 0) {
-				if (mat1.sum(&mat2) == 0) {
-					std::cout << "Сложение первой матрицы со второй прошла успешно\n";
-				}
-				else {
-					std::cout << "Размерность второй матрицы не совпадает с размерностью первой";
-				}
-			}
-			break;
-		}
-		case 12: {
-			if (mat1.get_columns() == 0) {
-				std::cout << "Матрица №1 пуста";
-				break;
-			}
-			if (mat1.trace() != 100000 ) {
-				std::cout << mat1.trace();
-			}
-			else {
-				std::cout << "Матрица №1 не квадратная\n";
-			}
-			break;
-		}
-		case 13: {
-			if (mat2.get_columns() == 0) {
-				std::cout << "Матрица №2 пуста";
-				break;
-			}
-			if (mat2.trace() != 100000) {
-				std::cout << mat2.trace();
-			}
-			else {
-				std::cout << "Матрица №2 не квадратная\n";
-			}
-			break;
-		}
-		case 14: {
-			if (mat1.mult(&mat2) == 0 or mat1.get_columns() == 0 or mat2.get_columns() == 0) {
-				std::cout << "Одна из матриц некорректна";
-			}
-			else {
-				mat1.mult(&mat2);
-				std::cout << "Умножение прошло успешно";
-			}
-			break;
-		}
-		case 15: {
-			if (mat2.mult(&mat1) == 0 or mat1.get_columns() == 0 or mat2.get_columns() == 0) {
-				std::cout << "Одна из матриц некорректна";
-			}
-			else {
-				mat2.mult(&mat1);
-				std::cout << "Умножение прошло успешно";
-			}
-			break;
-		}
-		case 16: {
 			return 0;
 		}
+		default:
+			std::cout << "Неверная команда\n";
+			continue;
 		}
 	}
+	return 0;
 }
--- lab5.1/Matrix.h
+++ lab5.1/Matrix.h
@@ -1,18 +1,32 @@
 #pragma once
-#include <iostream>
 class Matrix {
+private:
+	int row;
+	int col;
+	double* mat;
 public:
-	int size_col, size_row;
-	double* elem;
+	Matrix(int rows, int cols);
+	Matrix(int size);
 	Matrix();
+	Matrix(const Matrix& other);
 	~Matrix();
-	bool sum(const Matrix* mat2); //
-	bool mult(const Matrix* mat2);//
-	double trace(); //
-	void mult_by_num(double num); //
-	void input(); //
-	void print(); //
-	int get_columns(); //
-	int get_rows(); //
-	double get_elem(int i, int j); //
+
+	void input(int cols, int rows);
+	void input(int cols, int rows, double *mas);
+	void randinput(int cols, int rows);
+	void print();
+	void createmat(int cols, int rows);
+	int get_columns();
+	int get_rows();
+	double get_elem(int i, int j);
+	bool check_null(const Matrix& mat2);
+
+	Matrix sum(const Matrix mat2);
+	Matrix sum(double* mas, int size);
+	Matrix mult(const Matrix mat2);
+	Matrix mult(double* mas, int size);
+	double trace();
+	void shiftmat(Matrix& addmat, int index);
+	double det(int &a);
+	Matrix mult_by_num(double num);
 };
